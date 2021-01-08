a4fadd288983c97ede7cd4d589737b260bd3eca6
4masm <gabriel.wawerski@gmail.com>
2021-01-08 00:47:14 +0000

    simplify functions

--- src/scripts/board_calculator.py
+++ src/scripts/board_calculator.py
@@ -5,17 +5,17 @@
 # how many boxes on the pallet?
 # how many pallets out?
 def calc_print_user_board():
-    outers = input("Outers per case: ")
-    cases = input("Cases: ")
+    outers = int(input("Outers per case: "))
+    cases = int(input("Cases: "))
     print(f"Total outers: {_simple_calc_board(outers, cases)}")
 
 
 def _simple_calc_board(outers, cases):
-    return int(outers) * int(cases)
+    return outers * cases
 
 
 def _calc_board(outers, *cases):
     total_cases = 0
     for case in cases:
-        total_cases += int(case)
-    return int(outers) * int(total_cases)
+        total_cases += case
+    return outers * total_cases
